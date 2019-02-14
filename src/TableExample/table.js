d3921fa8fa5dcdb1ef7b997610dc0908546b78f5
israelcamp <israelcampiotti@gmail.com>
2019-02-14 21:16:27 -0200

    adding sorting to table

--- src/TableExample/table.js
+++ src/TableExample/table.js
@@ -7,78 +7,140 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Tooltip from '@material-ui/core/Tooltip';
 
-import { blue } from '@material-ui/core/colors';
+import { blue, amber } from '@material-ui/core/colors';
 import { objByString } from './utils';
 
-function DataTable(props) {
 
-  const { data, columns } = props;
+class DataTable extends React.Component {
 
-  const CustomTableCell = withStyles(theme => ({
-    head: {
-      backgroundColor: blue[500],
-      color: "white",
-      fontSize: 13,
-      ...props.headStyle
-    },
-    body: {
-      fontSize: 13,
-      ...props.bodyStyle
-    },
-  }))(TableCell);
-
-  const CustomPaper = withStyles(theme => ({
-    root: {
-      width: '100%',
-      overflowX: 'auto',
-      ...props.paperStyle
-    },
-  }))(Paper);
-
-  const CustomTableRow = withStyles(theme => ({
-    row: {
-      '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.background.default,
+  customs = {
+    CustomTableCell: withStyles(theme => ({
+      head: {
+        backgroundColor: blue[500],
+        color: "white",
+        fontSize: 13,
+        ...this.props.headStyle
       },
-      ...props.rowStyle
-    },
-  }))(TableRow);
-
-  for (const col of columns) {
-    if (!col.hasOwnProperty('cell')) col['cell'] = row => (
-      <div>
-        {objByString(row, col.selector)}
-      </div>
-    );
+      body: {
+        fontSize: 13,
+        ...this.props.bodyStyle
+      },
+    }))(TableCell),
+    CustomPaper: withStyles(theme => ({
+      root: {
+        width: '100%',
+        overflowX: 'auto',
+        ...this.props.paperStyle
+      },
+    }))(Paper),
+    CustomTableRow: withStyles(theme => ({
+      root: {
+        // '&:nth-of-type(odd)': {
+        //   backgroundColor: theme.palette.background.default,
+        // },
+        ...this.props.rowStyle
+      },
+    }))(TableRow),
+    CustomTableSortLabel: withStyles(theme => ({
+      root: {
+        '&:hover': {
+          color: amber[500],
+        },
+        '&:focus': {
+          color: amber[500]
+        },
+        ...this.props.sortStyle
+      },
+      active: {
+        color: amber[500],
+        ...this.props.sortStyle
+      }
+    }))(TableSortLabel)
   }
 
-  return (
-    <CustomPaper square={props.square}>
-      <Table padding={props.tablePadding}>
-        <TableHead>
-          <CustomTableRow hover>
-            {columns.map(col => (
-              <CustomTableCell key={col.name} align={props.alingHead}>
-                {col.name}
-              </CustomTableCell>
-            ))}
-          </CustomTableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row, index) => (
-            <CustomTableRow key={index} hover={props.hover}>
+  
+  constructor(props) {
+    super(props);
+    const { columns } = props;
+    for (const col of columns) {
+      if (!col.hasOwnProperty('cell')) col['cell'] = row => (
+        <div>
+          {objByString(row, col.selector)}
+        </div>
+      );
+      if (!col.hasOwnProperty('sorting')) col['sorting'] = (a, b) => {
+        const aa = objByString(a, col.selector);
+        const bb = objByString(b, col.selector);
+        if (aa < bb) return  1;
+        if (aa > bb) return -1;
+        return 0;
+      }
+    }
+    this.columns = columns;
+
+    this.state = {
+      order: 'desc',
+      orderBy: 0,
+    }
+
+  }
+
+  onOrderByChange = (order, colName) => {
+    const newOrder = (order === 'desc' && colName === this.state.orderBy) ? 'asc' : 'desc';
+    this.setState({ order: newOrder, orderBy: colName});
+  }
+
+  sortData = (order, orderBy) => this.props.data.sort((a,b) => order === 'asc' 
+      ? this.columns[orderBy].sorting(a,b)
+      :-this.columns[orderBy].sorting(a, b))
+  
+  render() {
+    const { props, columns } = this;
+    const { CustomTableCell, CustomPaper, CustomTableRow, CustomTableSortLabel } = this.customs;
+    const { order, orderBy } = this.state;
+    const sortedData = this.sortData(order, orderBy);
+    return (
+      <CustomPaper square={props.square}>
+        <Table padding={props.tablePadding}>
+          <TableHead>
+            <CustomTableRow hover>
               {columns.map((col, index) => (
-                <CustomTableCell key={index} align={props.alignRow}>
-                  {col.cell(row)}
+                <CustomTableCell key={col.name} align={props.alingHead}>
+                  <Tooltip
+                    title={`Sort by ${col.name}`}
+                    placement={'top-start'}
+                    enterDelay={props.delay}
+                  >
+                    <CustomTableSortLabel
+                      active={index === orderBy}
+                      direction={order}
+                      onClick={() => this.onOrderByChange(order, index)}
+                    >
+                      {col.name}
+                    </CustomTableSortLabel>
+                  </Tooltip>
                 </CustomTableCell>
               ))}
             </CustomTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </CustomPaper>
-  );
+          </TableHead>
+          <TableBody>
+            {sortedData.map((row, index) => (
+              <CustomTableRow key={index} hover={props.hover}>
+                {columns.map((col, index) => (
+                  <CustomTableCell key={index} align={props.alignRow}>
+                    {col.cell(row)}
+                  </CustomTableCell>
+                ))}
+              </CustomTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CustomPaper>
+    );
+  }
 }
 
 DataTable.propTypes = {
@@ -98,11 +160,13 @@ DataTable.defaultProps = {
   bodyStyle: {},
   paperStyle: {},
   rowStyle: {},
+  sortStyle: {},
   hover: true,
   square: false,
   alingHead: "center",
   alignRow: "center",
   tablePadding: "none",
+  delay: 500
 }
 
 export default DataTable;
