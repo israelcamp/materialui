1be775168db6f5989e4e76dcaeb764e30d140c32
israelcamp <israelcampiotti@gmail.com>
2020-02-08 14:17:09 -0300

    Updates

---
+++ precache-manifest.e94af8618f203be0d84ced63c6ee672b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6be77e3f84942f0f9253",
+    "url": "/materialui/static/css/main.9b471649.chunk.css"
+  },
+  {
+    "revision": "6be77e3f84942f0f9253",
+    "url": "/materialui/static/js/main.6cba7acf.chunk.js"
+  },
+  {
+    "revision": "1cb77a52169bc1d98abb",
+    "url": "/materialui/static/js/runtime~main.8d20bb69.js"
+  },
+  {
+    "revision": "fd4ceba74a318a1cac52",
+    "url": "/materialui/static/js/2.175544d1.chunk.js"
+  },
+  {
+    "revision": "20d4c6eba2b038fb33c5d8df06f2b30c",
+    "url": "/materialui/index.html"
+  }
+];
